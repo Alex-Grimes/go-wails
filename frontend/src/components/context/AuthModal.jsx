d8f71155286f749df087f291e698d16cf63c1d7c
alex <alexgrimes.se@gmail.com>
2023-04-01 14:38:31 -0400

    finishing up auth modal

--- frontend/src/components/context/AuthModal.jsx
+++ frontend/src/components/context/AuthModal.jsx
@@ -1,51 +1,56 @@
-import {Form, Input, Modal} from "antd";
-import {EyeInvisibleOutlines, EyeTwoTone} from "@ant-design/icons";
-import FormItem from "antd/es/form/FormItem";
+import { Form, Input, Modal } from 'antd';
+import {
+  EyeInvisibleOutlined,
+  EyeInvisibleOutlines,
+  EyeTwoTone,
+} from '@ant-design/icons';
 
-const AuthModal = ({shouldShowModal, onSubmit, onCancel}) => {
-    const [form] = Form.useForm();
+const AuthModal = ({ shouldShowModal, onSubmit, onCancel }) => {
+  const [form] = Form.useForm();
 
-    const onFormSubmit = () => {
-        form.validateFields().then((values) => {
-            onSubmit(values.token);
-        });
-    };
+  const onFormSubmit = () => {
+    form.validateFields().then((values) => {
+      onSubmit(values.token);
+    });
+  };
 
-    return (
-        <Modal
-        title ="Provide Github Auth Token"
-        centered
-        okText="Save"
-        cancelText="Cancel"
-        open={shouldShowModal}
-        onOk={onCancel}
-        onCancel={onCancel}
+  return (
+    <Modal
+      title='Provide Github Auth Token'
+      centered
+      okText='Save'
+      cancelText='Cancel'
+      open={shouldShowModal}
+      onOk={onCancel}
+      onCancel={onCancel}
+    >
+      <Form
+        form={form}
+        name='auth_form'
+        initialValues={{
+          token: '',
+        }}
+      >
+        <Form.Item
+          name='token'
+          label='Token'
+          rules={[
+            {
+              required: true,
+              message: 'Please provide your Github Token',
+            },
+          ]}
         >
+          <Input.Password
+            placeholder='Github Token'
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+          />
+        </Form.Item>
+      </Form>
+    </Modal>
+  );
+};
 
-            <Form
-                form={form}
-                name="auth_form"
-                initialValues={{
-                    token: "",
-                }}
-            >
-                <Form.Item
-                    name="token"
-                    label="Token"
-                    rules={[{
-                        required: true, message:"Please provide your Github Token",
-                    },]}
-                >
-                    <Input.Password 
-                        placeholder="Github Token"
-                        iconRender={(visible) => visible ? <EyeTwoTone/>}
-                    />
-
-                </Form.Item>
-
-            </Form>
-
-        </Modal>
-    )
-
-}
+export default AuthModal;
