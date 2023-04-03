51eebe47fc24967d7e78eb775f7eef688dc0acb5
alex <alexgrimes.se@gmail.com>
2023-04-03 13:13:16 -0400

    error handling

--- frontend/src/components/context/AuthContext.jsx
+++ frontend/src/components/context/AuthContext.jsx
@@ -20,4 +20,34 @@ const AuthContextProvider = ({ children }) => {
     }, 3600000);
     return () => clearTimeout(timer);
   }, [token]);
+
+  const onSubmit = (token) => {
+    setToken(token);
+    setShouldShowModal(false);
+  };
+
+  const onCancel = () => {
+    setShouldShowModal(false);
+  };
+
+  if (!shouldShowModal && !token) {
+    return (
+      <Result
+        status='error'
+        title='Authentication Failed'
+        subTitle='A Github token is required to view this page'
+        extra={[
+          <Button
+            type='link'
+            key='home'
+            onClick={() => {
+              navigate('/');
+            }}
+          >
+            Public Section
+          </Button>,
+        ]}
+      ></Result>
+    );
+  }
 };
